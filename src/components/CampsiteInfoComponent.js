0f8c01a8df88a719efabfeb05139129ca8b7ff9f
Chet Carter <abazabalv@gmail.com>
2020-06-15 19:46:17 -0600

    Redux Actions

--- src/components/CampsiteInfoComponent.js
+++ src/components/CampsiteInfoComponent.js
@@ -39,7 +39,12 @@ class CommentForm extends Component {
 
   handleSubmit(values) {
     this.toggleModal();
-    alert(`Rating: ${values.rating} Author: ${values.author} Comment: ${values.text}`);
+    this.props.addComment(
+      this.props.campsiteId,
+      values.rating,
+      values.author,
+      values.text
+    );
   }
 
   render() {
@@ -53,67 +58,61 @@ class CommentForm extends Component {
           <ModalBody>
             <LocalForm onSubmit={(values) => this.handleSubmit(values)}>
               <div className="form-group">
-                <Label htmlFor="rating">
-                  Rating
-                </Label>
-                  <Control.select
-                    model=".rating"
-                    id="rating"
-                    name="rating"
-                    className="form-control"
-                    defaultValue="1"
-                  >
-                    <option>1</option>
-                    <option>2</option>
-                    <option>3</option>
-                    <option>4</option>
-                    <option>5</option>
-                  </Control.select>
+                <Label htmlFor="rating">Rating</Label>
+                <Control.select
+                  model=".rating"
+                  id="rating"
+                  name="rating"
+                  className="form-control"
+                  defaultValue="1"
+                >
+                  <option>1</option>
+                  <option>2</option>
+                  <option>3</option>
+                  <option>4</option>
+                  <option>5</option>
+                </Control.select>
               </div>
               <div className="form-group">
-                <Label htmlFor="author">
-                  Your Name
-                </Label>
-                  <Control.text
-                    model=".author"
-                    id="author"
-                    name="author"
-                    placeholder="Your Name"
-                    className="form-control"
-                    validators={{
-                        required,
-                        minLength: minLength(2),
-                        maxLength: maxLength(15)
-                    }}
-                  />
-                  <Errors
-                    className="text-danger"
-                    model=".author"
-                    show="touched"
-                    component="div"
-                    messages={{
-                      required:'Required',
-                      minLength:'Must be at least 2 characters',
-                      maxLength:'Must be 15 characters or less'
-                    }}
-                  />
+                <Label htmlFor="author">Your Name</Label>
+                <Control.text
+                  model=".author"
+                  id="author"
+                  name="author"
+                  placeholder="Your Name"
+                  className="form-control"
+                  validators={{
+                    required,
+                    minLength: minLength(2),
+                    maxLength: maxLength(15),
+                  }}
+                />
+                <Errors
+                  className="text-danger"
+                  model=".author"
+                  show="touched"
+                  component="div"
+                  messages={{
+                    required: "Required",
+                    minLength: "Must be at least 2 characters",
+                    maxLength: "Must be 15 characters or less",
+                  }}
+                />
               </div>
               <div className="form-group">
-                <Label htmlFor="text">
-                  Comment
-                </Label>
-                  <Control.textarea
-                    model=".text"
-                    id="text"
-                    name="text"
-                    rows="6"
-                    className="form-control"
-                  />
+                <Label htmlFor="text">Comment</Label>
+                <Control.textarea
+                  model=".text"
+                  id="text"
+                  name="text"
+                  rows="6"
+                  className="form-control"
+                />
               </div>
               <div className="form-group">
-                  <Button type="submit" color="primary">
-                    Submit
-                  </Button>
+                <Button type="submit" color="primary">
+                  Submit
+                </Button>
               </div>
             </LocalForm>
           </ModalBody>
@@ -136,7 +135,7 @@ function RenderCampsite({ campsite }) {
   );
 }
 
-function RenderComments({ comments }) {
+function RenderComments({ comments, addComment, campsiteId }) {
   if (comments) {
     return (
       <div className="col-md-5 m-1">
@@ -156,7 +155,7 @@ function RenderComments({ comments }) {
             </div>
           );
         })}
-        <CommentForm />
+        <CommentForm campsiteId={campsiteId} addComment={addComment} />
       </div>
     );
   }
@@ -180,7 +179,11 @@ function CampsiteInfo(props) {
         </div>
         <div className="row">
           <RenderCampsite campsite={props.campsite} />
-          <RenderComments comments={props.comments} />
+          <RenderComments
+            comments={props.comments}
+            addComment={props.addComment}
+            campsiteId={props.campsite.id}
+          />
         </div>
       </div>
     );
